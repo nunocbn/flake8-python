4c7780a7809e9781aec6a5b2fffe78091e78cf57
Nuno Nogueira <nunocbnn@gmail.com>
2021-08-25 18:33:51 -0300

    Criando Métodos Abstratos na Classe FilaBase

--- fila_normal.py
+++ fila_normal.py
@@ -2,15 +2,15 @@ from fila_base import FilaBase
 
 
 class FilaNormal(FilaBase):
-    def gerasenhaatual(self) -> None:
-        self.senhaatual = f"NM{self.codigo}"
+    def gera_senha_atual(self) -> None:
+        self.senha_atual = f"NM{self.codigo}"
 
-    def atualizafila(self) -> None:
+    def atualiza_fila(self) -> None:
         self.reseta_fila()
-        self.gerasenhaatual()
-        self.fila.append(self.senhaatual)
+        self.gera_senha_atual()
+        self.fila.append(self.senha_atual)
 
-    def chamacliente(self, caixa: int) -> str:
+    def chama_cliente(self, caixa: int) -> str:
         cliente_atual: str = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return f"Cliente {cliente_atual} dirija-se ao Caixa {caixa}"
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fila_normal import FilaNormal
 from fila_prioritaria import FilaPrioritaria
 
+
 # fila_test = FilaNormal()
-# fila_test.atualizafila()
-# fila_test.atualizafila()
-# fila_test.atualizafila()
-# print(fila_test.chamacliente(5))
-# print(fila_test.chamacliente(2))
+# fila_test.atualiza_fila()
+# fila_test.atualiza_fila()
+# fila_test.atualiza_fila()
+# print(fila_test.chama_cliente(5))
+# print(fila_test.chama_cliente(2))
 
 fila_test_2 = FilaPrioritaria()
 fila_test_2.atualiza_fila()
@@ -14,4 +15,4 @@ fila_test_2.atualiza_fila()
 fila_test_2.atualiza_fila()
 print(fila_test_2.chama_cliente(10))
 print(fila_test_2.chama_cliente(1))
-print(fila_test_2.estatisca("21/08/2021", 196, "qualquer string"))
+print(fila_test_2.estatisca("21/08/2021", 196, "detail"))
