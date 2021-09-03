f5aa138f89e766603a668d8c8e7a2e9993e70a08
Nuno Nogueira <nunocbnn@gmail.com>
2021-09-03 00:08:44 -0300

    Criando Arquivo e Classe FabricaFila

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fila_normal import FilaNormal
 from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
 
 # fila_test = FilaNormal()
@@ -9,10 +10,16 @@ from fila_prioritaria import FilaPrioritaria
 # print(fila_test.chama_cliente(5))
 # print(fila_test.chama_cliente(2))
 
-fila_test_2 = FilaPrioritaria()
-fila_test_2.atualiza_fila()
-fila_test_2.atualiza_fila()
-fila_test_2.atualiza_fila()
-print(fila_test_2.chama_cliente(10))
-print(fila_test_2.chama_cliente(1))
-print(fila_test_2.estatisca("21/08/2021", 196, "detail"))
+# fila_test_2 = FilaPrioritaria()
+# fila_test_2.atualiza_fila()
+# fila_test_2.atualiza_fila()
+# fila_test_2.atualiza_fila()
+# print(fila_test_2.chama_cliente(10))
+# print(fila_test_2.chama_cliente(1))
+# print(fila_test_2.estatisca("21/08/2021", 196, "detail"))
+
+teste_fabrica = FabricaFila.escolhe_fila("normal")
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
